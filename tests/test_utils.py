968e9fa8864b44cab3f1f63393e1bdcf1ab7c48b
Piët Delport <pjdelport@gmail.com>
2016-03-01 15:18:56 +0200

    Test utils.is_candidate_name()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,8 @@
+import string
+
+from hypothesis import given, example, note, assume
+from hypothesis.strategies import text
+
 from py2_compat import unittest, mock
 
 from django_develop import utils
@@ -31,3 +36,59 @@ class TestVirtualEnvDetection(unittest.TestCase):
                 with mock.patch('django_develop.utils.sys', spec=[], **sys_attrs):
                     expected = sys_attrs['prefix'] is virtual
                     self.assertEqual(utils.is_inside_virtual_env(), expected)
+
+
+class TestIsCandidateName(unittest.TestCase):
+    """
+    `utils.is_candidate_name()`
+    """
+
+    # Hacky strategy for module names
+    modnames = text(string.printable).map(str)
+
+    @given(modnames.filter(lambda s: 'settings' not in s))
+    @example('not_a_setting')
+    def test_non_candidates(self, modname):
+        """
+        Random names aren't candidates.
+        """
+        self.assertFalse(utils.is_candidate_name(modname))
+
+    @given(modnames, modnames)
+    @example('foo_', '_bar')
+    @example('app.', '.mod')
+    def test_candidates(self, pre, post):
+        """
+        Names containing "settings" are candidates.
+        """
+        modname = pre + 'settings' + post
+        note('modname={!r}'.format(modname))
+        self.assertTrue(utils.is_candidate_name(modname))
+
+    # Special-cased names
+    specials = [
+        'django.conf.global_settings',
+        'django.core.management.commands.diffsettings',
+        'django_develop.dev_settings',
+    ]
+
+    def test_exceptions(self):
+        """
+        The special-cased names are not candidates.
+        """
+        for modname in self.specials:
+            with self.subTest(modname=modname):
+                self.assertFalse(utils.is_candidate_name(modname))
+
+    @given(modnames, modnames)
+    @example('foo_', '_bar')
+    @example('app.', '.mod')
+    def test_modified_exceptions(self, pre, post):
+        """
+        Prefixed and suffixed versions of the special-cased names are not excepted.
+        """
+        assume(pre or post)  # Require at least some prefix or suffix
+        for special in self.specials:
+            modname = pre + special + post
+            note('modname={!r}'.format(modname))
+            self.assertTrue(utils.is_candidate_name(modname))
