d594c3162288f01501ce3049420d41a1274a923e
Piët Delport <pjdelport@gmail.com>
2016-03-10 02:56:39 +0200

    Begin testing DjangoDevelop

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 from io import StringIO
+from pathlib import Path
 
-from py2_compat import unittest, mock
+from py2_compat import unittest, mock, TemporaryDirectory
 
 from django_develop import cli
 
@@ -8,6 +9,26 @@ from django_develop import cli
 def _patch_inside_virtual_env(expected):
     return mock.patch('django_develop.utils.is_inside_virtual_env',
                       mock.Mock(return_value=expected))
+
+
+class TestDjangoDevelop(unittest.TestCase):
+
+    def setUp(self):
+        # Get a temporary instance directory
+        temp_dir = TemporaryDirectory()
+        self.instance_dir = temp_dir.name
+        self.addCleanup(temp_dir.cleanup)
+
+        self.dd = cli.DjangoDevelop(self.instance_dir)
+
+    def test_read_write_config(self):
+        config_path = Path(self.instance_dir, 'django-develop.ini')
+
+        self.assertFalse(config_path.exists())
+        config = self.dd.read_config()
+        self.dd.write_config(config)
+        self.assertTrue(config_path.is_file())
+        self.assertEqual(config_path.stat().st_size, 0)
 
 
 @mock.patch('sys.stderr', new_callable=StringIO)
